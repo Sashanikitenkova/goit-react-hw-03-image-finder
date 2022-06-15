b36c562adbfe236429183d8d76d0b337ca3471da
Sashanikitenkova <dolotova.a2017@gmail.com>
2022-06-15 23:53:31 +0300

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
-export const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+import React, {Component} from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Searchbar} from "./Searchbar/Searchbar";
+import {ImageGallery} from "./ImageGallery/ImageGallery";
+import s from './App.module.css';
+
+export class App extends Component {
+  state = {
+    imageName:'',
+  }
+
+  handleFormSubmit = imageName => {
+    this.setState({imageName});
+  }
+
+  render () {
+    const {imageName} = this.state;
+
+    return (
+        <div className={s.App}>
+          <Searchbar onSubmit={this.handleFormSubmit} />
+          <ImageGallery imageName={imageName} />
+          <ToastContainer autoClose={3000} />
+        </div>
+    );
+  }
 };
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import s from './ImageGalleryItem.module.css'
+
+const ImageGalleryItem = ({id, webformatURL, largeImageURL, tags, onClick}) => {
+
+    return (
+                       <li key={id} onClick={() => onClick(largeImageURL)} className={s.ImageGalleryItem}>
+                           <img className={s.ImageGalleryItemImage} src={webformatURL} alt={tags} width="300"  />
+                       </li>
+    )
+} 
+
+ImageGalleryItem.propTypes = {
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+      onClick: PropTypes.func.isRequired,
+}
+
+export default ImageGalleryItem;
