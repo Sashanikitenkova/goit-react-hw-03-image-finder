a503de30b58f0b7990130b1821338fa14cb29bd4
Sashanikitenkova <dolotova.a2017@gmail.com>
2022-07-09 00:25:43 +0300

    end1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,120 @@
 import React, {Component} from "react";
-import { ToastContainer } from 'react-toastify';
+import { fetchData } from "services/imagesApi";
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {Searchbar} from "./Searchbar/Searchbar";
+import Loader from "./Loader/Loader";
 import {ImageGallery} from "./ImageGallery/ImageGallery";
+import Button from "./Button/Button";
+import { Modal } from "./Modal/Modal";
 import s from './App.module.css';
 
 export class App extends Component {
   state = {
     imageName:'',
+    imageSearch: [],
+    isLoading: false,
+    error: null,
+    page: 1,
+    showModal: false,
+    largeImageURL: null,
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { imageName, page } = this.state;
+
+    if(prevState.imageName !== imageName || (prevState.page !== page && page !== 1)) {
+        this.fetchImages();
+    }
+}
+
+fetchImages = () => {
+  const { imageName, page } = this.state;
+  const perPage = 12;
+
+  this.setState({ isLoading: true });
+
+  fetchData(imageName, page, perPage)
+  .then(({ hits, totalHits }) => {
+    const totalPages = Math.ceil(totalHits / perPage);
+
+    if (hits.length === 0) {
+      return toast('Sorry, no images found. Please, try again!');
+    }
+
+    if (page === 1) {
+      toast(`Hooray! We found ${totalHits} images.`);
+    }
+
+    if (page === totalPages) {
+      toast("You've reached the end of search results.");
+    }
+
+    const data = hits.map(({id, webformatURL, largeImageURL, tags}) => {
+      return {
+        id,
+        webformatURL,
+        largeImageURL,
+        tags,
+      };
+    });
+
+    this.setState(({imageSearch}) => ({
+      imageSearch: [...imageSearch, ...data],
+      total: totalHits,
+    }))
+  })
+  .catch(error => this.setState({ error }))
+  .finally(() => this.setState({ isLoading: false }));
+}
 
   handleFormSubmit = imageName => {
-    this.setState({imageName});
+    if (imageName === this.state.imageName) return;
+    this.setState({
+      imageSearch: [],
+      imageName,
+      page: 1,
+      error: null,
+    });
   }
 
+  onLoadMore = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+      isLoading: true,
+    }));
+  };
+
+  toggModal = picture => {
+    this.setState(({showModal}) => ({ showModal: !showModal }));
+    this.setState({ largeImageURL: picture });
+ }
+
   render () {
-    const {imageName} = this.state;
+    const { imageSearch, error, isLoading, showModal, largeImageURL, tags, total } = this.state;
+    const loadImages = imageSearch.length !== 0;
+    const isLastPage = imageSearch.length === total;
+    const loadMoreBtn = loadImages && !isLoading && !isLastPage;
 
     return (
         <div className={s.App}>
           <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery imageName={imageName} />
+
+          {error && toast(error.message)}
+
+          {isLoading && <Loader />}
+
+          {loadImages && (
+          <ImageGallery imageSearch={imageSearch} onClick={this.toggModal} />)}
+          
+          {loadMoreBtn &&
+          <Button onClick={this.onLoadMore}/> }
+          
+          {showModal && <Modal onClose={this.toggModal}><img src={largeImageURL} alt={tags} /></Modal>} 
+
           <ToastContainer autoClose={3000} />
         </div>
     );
   }
 };
+
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,78 +1,11 @@
-import React, {Component} from "react";
+import PropTypes from 'prop-types';
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
-import Button from "components/Button/Button";
-import Loader from "components/Loader/Loader";
-import { Modal } from "components/Modal/Modal";
 import s from './ImageGallery.module.css'
 
-export class ImageGallery extends Component {
-    state = {
-        imageSearch: [],
-        largeImageURL: null,
-        page: 1,
-        error: null,
-        showModal: false,
-        status: 'idle',
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if(prevProps.imageName !== this.props.imageName) {
-            this.setState({ status: 'pending', imageSearch: []})
-            this.fetchImages();
-        }
-    }
-
-    fetchImages = () => {
-        const {page} = this.state;
-
-        this.setState({ status: 'pending'})
-
-            fetch(`https://pixabay.com/api/?key=25749295-c1c91c3a002bacdc6232fef3b&q=${this.props.imageName}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`)
-            .then(response => {
-                if (response.ok) {
-                   return response.json();
-                }
-
-                return Promise.reject(
-                    new Error(`No photos on request ${this.props.imageName}`),
-                );
-            }
-            )
-            .then(({hits}) => {
-                  this.setState(({imageSearch}) => ({
-                  imageSearch: [...imageSearch, ...hits],
-                  page: page+1,
-                  status: 'resolved',
-             }))
-           });
-        //    .cath(error => this.setState({error, status: 'rejected'}));
-          
-    }
-
-    toggModal = picture => {
-       this.setState(({showModal}) => ({ showModal: !showModal }));
-       this.setState({ largeImageURL: picture });
-    }
-  
-    render () {
-        const { imageSearch, largeImageURL, error, showModal, status } = this.state;
-
-        if(status === 'idle') {
-            return 
-            // <div>Введите имя фотографии</div>
-        }
-
-        if(status === 'pending') {
-            return <Loader />
-        }
-
-        if(status === 'rejected') {
-            return <h1>{error.massage}</h1>
-        }
-
-        if(status === 'resolved') {
-            return <>
-                     <ul className={s.ImageGallery}>
+export const ImageGallery = ({imageSearch, onClick}) => {
+   
+            return (
+                <ul className={s.ImageGallery}>
                          {imageSearch.map(({id, webformatURL, largeImageURL, tags}) => (
                              <ImageGalleryItem 
                                   key={id}
@@ -80,19 +13,28 @@ export class ImageGallery extends Component {
                                   webformatURL={webformatURL}
                                   largeImageURL={largeImageURL}
                                   tags={tags}
-                                  onClick={this.toggModal}
+                                  onClick={onClick}
                              />
                          ))}
-                     </ul>  
-                     <Button onClick={this.fetchImages}/> 
-                     {showModal && <Modal onClose={this.toggModal}><img src={largeImageURL} alt="" /></Modal>} 
-                   </> 
-        }
-    }
+                </ul>  
+            )
   };
 
-  
+ImageGallery.propTypes = {
+    imageSearch: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        webformatURL: PropTypes.string.isRequired,
+        largeImageURL: PropTypes.string.isRequired,
+        tags: PropTypes.string.isRequired,
+      })
+    ),
+    onClick: PropTypes.func.isRequired,
+};
 
+
+
+  
 
 
 
